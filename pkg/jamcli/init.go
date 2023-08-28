36ca4985b676304a4bd4a4ceea15ac3dced6b161
Zachary Geier <zdgeier@gmail.com>
2023-08-27 23:16:29 -0700

    init

--- pkg/jamcli/init.go
+++ pkg/jamcli/init.go
@@ -63,14 +63,14 @@ func InitNewProject(conn *grpc.ClientConn, projectName string) {
 		panic(err)
 	}
 
-	// _, err = apiClient.DeleteWorkspace(context.Background(), &jampb.DeleteWorkspaceRequest{
-	// 	OwnerUsername: resp.GetOwnerUsername(),
-	// 	ProjectId:     resp.GetProjectId(),
-	// 	WorkspaceId:   workspaceResp.WorkspaceId,
-	// })
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
+	_, err = apiClient.DeleteWorkspace(context.Background(), &jampb.DeleteWorkspaceRequest{
+		OwnerUsername: resp.GetOwnerUsername(),
+		ProjectId:     resp.GetProjectId(),
+		WorkspaceId:   workspaceResp.WorkspaceId,
+	})
+	if err != nil {
+		log.Panic(err)
+	}
 
 	err = statefile.StateFile{
 		OwnerUsername: resp.OwnerUsername,
